19de11e5541ecdd35dac63c431e4741d3024bf8d
Matheus Vitoriano <matheusbzv@gmail.com>
2024-07-05 14:30:17 -0300

    Cleanup

--- js/main.js
+++ js/main.js
@@ -16,10 +16,6 @@ $(window).on('scroll', function () {
 	}
 });
 
-
-
-
-
 $(document).ready(function(){
 
 // mobile_menu
@@ -35,41 +31,6 @@ if(menu.length){
   // $('ul#blog-menu').slicknav({
   //   prependTo: ".blog_menu"
   // });
-
-// review-active
-$('.slider_active').owlCarousel({
-  loop:true,
-  margin:0,
-items:1,
-autoplay:true,
-navText:['<i class="ti-angle-left"></i>','<i class="ti-angle-right"></i>'],
-  nav:true,
-dots:false,
-autoplayHoverPause: true,
-autoplaySpeed: 800,
-  responsive:{
-      0:{
-          items:1,
-          nav:false,
-      },
-      767:{
-          items:1,
-          nav:false,
-      },
-      992:{
-          items:1,
-          nav:false
-      },
-      1200:{
-          items:1,
-          nav:false
-      },
-      1600:{
-          items:1,
-          nav:true
-      }
-  }
-});
 
 
   // scrollIt for smoth scroll
